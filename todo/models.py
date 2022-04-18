046b0d6ea21bbf2099efd3e689f69826f36297c8
storrer <storrer.chris@gmail.com>
2022-04-17 20:52:29 -0700

    Add completion field to models.py

--- todo/models.py
+++ todo/models.py
@@ -5,3 +5,5 @@ from django.db import models
 class Task(models.Model):
     # An ID field is automatically added to all Django models
     description = models.CharField(max_length=255)
+    # This Boolean field is used to mark a task as completed
+    completed = models.BooleanField(default=False)
